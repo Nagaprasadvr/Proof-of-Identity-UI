8c985539d9d2a7a6519c2342a7e73032c271f472
Nagaprasad v r <nagaprasadvr246@gmail.com>
2022-11-14 20:25:16 +0530

    wallet ..

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,26 @@
-import React from "react";
-import { InputForm } from "../InputForm/InputForm";
-import "../../App.css";
+import React from 'react';
+import { InputForm } from '../InputForm/InputForm';
+import '../../App.css';
+import { useWallet } from '@solana/wallet-adapter-react';
 export const Home = () => {
-  return (
-    <div className="App">
-      <h1 className="w3-animate-opacity">
-        <b>Create your Digital identity on Solana chain!</b>
-      </h1>
-      <div className="w3-animate-opacity w3-jumbo">
-        <i className="fa fa-fingerprint" style={{ color: "lightskyblue" }}></i>
-      </div>
-      <InputForm />
-    </div>
-  );
+    const user = useWallet();
+    return (
+        <div className="App">
+            <h1 className="w3-animate-opacity">
+                <b>Create your Digital identity on Solana chain!</b>
+            </h1>
+            {user ? (
+                <h1 className="w3-animate-opacity">
+                    <b>make your fingerprint on solana {user}!</b>
+                </h1>
+            ) : (
+                <h1 className="w3-animate-opacity">Connect Your Wallet !</h1>
+            )}
+
+            <div className="w3-animate-opacity w3-jumbo">
+                <i className="fa fa-fingerprint" style={{ color: 'lightskyblue' }}></i>
+            </div>
+            <InputForm />
+        </div>
+    );
 };
