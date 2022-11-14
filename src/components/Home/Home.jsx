ef42d18762ce14589b80fd846a61a62b3797ffa0
Nagaprasad V R <81755170+Nagaprasadvr@users.noreply.github.com>
2022-11-14 23:36:59 +0530

    Change

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,13 +9,7 @@ export const Home = () => {
             <h1 className="w3-animate-opacity">
                 <b>Create your Digital identity on Solana chain!</b>
             </h1>
-            {user ? (
-                <h1 className="w3-animate-opacity">
-                    <b>make your fingerprint on solana {user}!</b>
-                </h1>
-            ) : (
-                <h1 className="w3-animate-opacity">Connect Your Wallet !</h1>
-            )}
+           
 
             <div className="w3-animate-opacity w3-jumbo">
                 <i className="fa fa-fingerprint" style={{ color: 'lightskyblue' }}></i>
