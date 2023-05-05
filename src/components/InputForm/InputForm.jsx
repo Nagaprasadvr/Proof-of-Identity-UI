c576198ef005ec04790fdb03fee7e95c97f7e033
HARSHAVARDHAN2K <vardhanvardhanthi@gmail.com>
2023-05-05 22:14:38 +0530

    Quill forms setup is done

--- src/components/InputForm/InputForm.jsx
+++ src/components/InputForm/InputForm.jsx
@@ -1,99 +1,95 @@
-// import { PopupButton } from "@typeform/embed-react";
-// import React from "react";
-
-
-// import { Form } from "@quillforms/renderer-core";
-// import "@quillforms/renderer-core/build-style/style.css";
 import { Form } from "@quillforms/renderer-core";
 import { Button } from "@mui/material"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./InputForm.css";
 import "@quillforms/renderer-core/build-style/style.css";
-// import { registerCoreBlocks } from "@quillforms/react-renderer-utils";
 import { useState } from "react";
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import { registerCoreBlocks } from "@quillforms/react-renderer-utils";
+registerCoreBlocks();
 
 // registerCoreBlocks();
 export const InputForm = () => {
-    const [form, openForm] = useState(false);
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     return (
         <>
-            <button style={{ width: "auto" }} className="balance-button w3-btn w3-hover-white App" onClick={() => { openForm(!form) }}>
-                Create Identity
-            </button>
+            <Button variant="contained" onClick={handleOpen}>Create TAG</Button>
 
-
-
-            {form ?
-
-                (<div style={{ width: "800px", height: "800px", paddingTop: "20px", }} >
-                    <Form
-                        style={{ borderRadius: "10px" }}
-                        formId="1"
-                        formObj={{
-                            blocks: [
-                                {
-                                    name: "welcome-screen",
-                                    id: "jg1401r",
-                                    attributes: {
-                                        label: "Welcome to our survey",
-                                        description: "This is just a description",
-                                        attachment: {
-                                            type: "image",
-                                            url:
-                                                "https://quillforms.com/wp-content/uploads/2022/01/4207-ai-1.jpeg"
-                                        }
-                                    }
-                                },
-                                {
-                                    name: "short-text",
-                                    id: "kd12edg",
-                                    attributes: {
-                                        classnames: "first-block",
-                                        required: true,
-                                        label: "Let's start with your name"
-                                    }
-                                },
-                                {
-                                    name: "multiple-choice",
-                                    id: "gqr1294c",
-                                    attributes: {
-                                        required: true,
-                                        multiple: true,
-                                        verticalAlign: false,
-                                        label: "Which subjects do you love the most?",
-                                        choices: [
-                                            {
-                                                label: "Physics",
-                                                value: "physics"
-                                            },
-                                            {
-                                                label: "Math",
-                                                value: "math"
-                                            },
-                                            {
-                                                label: "English",
-                                                value: "english"
-                                            },
-                                            {
-                                                label: "Biology",
-                                                value: "biology"
+            <Modal open={open} onClose={handleClose} style={{ borderRadius: "50px" }}>
+                <Box>
+                    {
+                        open && (<div style={{ width: "90%", height: "90vh", marginTop: "5vh", marginLeft: "5%" }}>
+                            <Form
+                                formId="1"
+                                formObj={{
+                                    blocks: [
+                                        {
+                                            name: "welcome-screen",
+                                            id: "jg1401r",
+                                            attributes: {
+                                                label: "Welcome to our survey",
+                                                description: "This is just a description",
+                                                attachment: {
+                                                    type: "image",
+                                                    url:
+                                                        "https://quillforms.com/wp-content/uploads/2022/01/4207-ai-1.jpeg"
+                                                }
                                             }
-                                        ]
-                                    }
-                                },
-                            ],
-                        }}
-                        onSubmit={(data, { completeForm, setIsSubmitting, goToBlock, setSubmissionErr }) => {
-                            setTimeout(() => {
-                                setIsSubmitting(false);
-                                completeForm();
-                            }, 500);
-                        }}
-                    /></div >) : null}
-
-
-
+                                        },
+                                        {
+                                            name: "short-text",
+                                            id: "kd12edg",
+                                            attributes: {
+                                                classnames: "first-block",
+                                                required: true,
+                                                label: "Let's start with your name"
+                                            }
+                                        },
+                                        {
+                                            name: "multiple-choice",
+                                            id: "gqr1294c",
+                                            attributes: {
+                                                required: true,
+                                                multiple: true,
+                                                verticalAlign: false,
+                                                label: "Which subjects do you love the most?",
+                                                choices: [
+                                                    {
+                                                        label: "Physics",
+                                                        value: "physics"
+                                                    },
+                                                    {
+                                                        label: "Math",
+                                                        value: "math"
+                                                    },
+                                                    {
+                                                        label: "English",
+                                                        value: "english"
+                                                    },
+                                                    {
+                                                        label: "Biology",
+                                                        value: "biology"
+                                                    }
+                                                ]
+                                            }
+                                        },
+                                    ],
+                                }}
+                                onSubmit={(data, { completeForm, setIsSubmitting, goToBlock, setSubmissionErr }) => {
+                                    setTimeout(() => {
+                                        setIsSubmitting(false);
+                                        completeForm();
+                                    }, 500);
+                                }}
+                            />
+                        </div>)
+                    }
+                </Box>
+            </Modal>
         </>
     );
 };
