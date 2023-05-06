d8ee648f008d91b4ab77758f8fcadfd7b24b2ae6
HARSHAVARDHAN2K <vardhanvardhanthi@gmail.com>
2023-05-06 11:18:15 +0530

    partial form

--- src/components/InputForm/InputForm.jsx
+++ src/components/InputForm/InputForm.jsx
@@ -15,6 +15,7 @@ export const InputForm = () => {
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
+
     return (
         <>
             <Button variant="contained" onClick={handleOpen}>Create TAG</Button>
@@ -22,7 +23,8 @@ export const InputForm = () => {
             <Modal open={open} onClose={handleClose} style={{ borderRadius: "50px" }}>
                 <Box>
                     {
-                        open && (<div style={{ width: "90%", height: "90vh", marginTop: "5vh", marginLeft: "5%" }}>
+                        open && (<div style={{ width: "100%", height: "100vh" }}>
+
                             <Form
                                 formId="1"
                                 formObj={{
@@ -37,49 +39,152 @@ export const InputForm = () => {
                                                     type: "image",
                                                     url:
                                                         "https://quillforms.com/wp-content/uploads/2022/01/4207-ai-1.jpeg"
-                                                }
+                                                },
+                                                attachmentMaxWidth: "300px"
                                             }
                                         },
                                         {
                                             name: "short-text",
                                             id: "kd12edg",
                                             attributes: {
-                                                classnames: "first-block",
+                                                attachment: {
+                                                    type: "image",
+                                                    url:
+                                                        "https://quillforms.com/wp-content/uploads/2022/10/ludovic-migneault-B9YbNbaemMI-unsplash_50-scaled.jpeg"
+                                                },
+                                                layout: "split-right",
                                                 required: true,
-                                                label: "Let's start with your name"
+                                                label: "Your Solana Wallet Address (Pubkey) ?",
+                                                description: "please enter a valid Solana address!",
                                             }
                                         },
                                         {
-                                            name: "multiple-choice",
-                                            id: "gqr1294c",
+                                            name: "short-text",
+                                            id: "wer3qdkdb",
                                             attributes: {
                                                 required: true,
-                                                multiple: true,
-                                                verticalAlign: false,
-                                                label: "Which subjects do you love the most?",
-                                                choices: [
-                                                    {
-                                                        label: "Physics",
-                                                        value: "physics"
-                                                    },
-                                                    {
-                                                        label: "Math",
-                                                        value: "math"
-                                                    },
-                                                    {
-                                                        label: "English",
-                                                        value: "english"
-                                                    },
-                                                    {
-                                                        label: "Biology",
-                                                        value: "biology"
-                                                    }
-                                                ]
+                                                label: "Enter your Sol NameService domain if any..."
                                             }
                                         },
+                                        {
+                                            name: "number",
+                                            id: "3nsdf934",
+                                            attributes: {
+                                                label: "Enter your Phone Number...",
+                                                required: true,
+
+                                                "max": 10
+                                            }
+                                        },
+                                        {
+                                            id: "1dsdf12e",
+                                            name: "group",
+                                            attributes: {
+                                                label: "Your Resedential Address.."
+                                                , description: "enter your present address"
+                                            },
+                                            innerBlocks: [
+                                                {
+                                                    id: "asfijais1e",
+                                                    name: "short-text",
+                                                    attributes: {
+                                                        label: "address",
+                                                        required: true,
+                                                        placeholder: "Street and cross"
+                                                    }
+                                                },
+                                                {
+                                                    id: "7dsjsdv821",
+                                                    name: "short-text",
+                                                    attributes: {
+                                                        label: "Address Line 2",
+                                                        required: true,
+                                                        placeholder: "Area and Post"
+                                                    }
+                                                },
+                                                {
+                                                    id: "2esad013x",
+                                                    name: "short-text",
+                                                    attributes: {
+                                                        label: "City/Town",
+                                                        required: true,
+                                                        placeholder: "Tumkur"
+                                                    }
+                                                }, {
+
+                                                    name: "short-text",
+                                                    attributes: {
+                                                        label: " State/Region/Province",
+                                                        required: true,
+                                                        placeholder: "Karnataka"
+                                                    }
+                                                }, {
+                                                    id: "2esad013c",
+                                                    name: "number",
+                                                    attributes: {
+                                                        label: "zip / Post code",
+                                                        required: true,
+                                                        max: "10",
+                                                        placeholder: "572102"
+                                                    }
+                                                }, {
+                                                    id: "2esad013b",
+                                                    name: "short-text",
+                                                    attributes: {
+                                                        label: "Country",
+                                                        required: true,
+                                                        placeholder: "India"
+                                                    }
+                                                }
+                                            ]
+                                        }, {
+                                            id: "2esad013a",
+                                            name: "email",
+                                            attributes: {
+                                                label: "Email",
+                                                required: true,
+                                                placeholder: "Write your email here!"
+                                            }
+                                        }, {
+                                            name: "date",
+                                            id: "a213rsew",
+                                            attributes: {
+                                                required: true,
+                                                label: "Please type your birth of date!"
+                                            }
+                                        }, {
+
+                                        }
+
+
+
+
+
+
                                     ],
+                                    settings: {
+                                        animationDirection: "vertical",
+                                        disableWheelSwiping: false,
+                                        disableNavigationArrows: false,
+                                        disableProgressBar: false
+                                    },
+                                    theme: {
+                                        font: "Roboto",
+                                        buttonsBgColor: "#9b51e0",
+                                        logo: {
+                                            src: ""
+                                        },
+                                        questionsColor: "#000",
+                                        answersColor: "#0aa7c2",
+                                        buttonsFontColor: "#fff",
+                                        buttonsBorderRadius: 25,
+                                        errorsFontColor: "#fff",
+                                        errorsBgColor: "#f00",
+                                        progressBarFillColor: "#000",
+                                        progressBarBgColor: "#ccc"
+                                    }
                                 }}
-                                onSubmit={(data, { completeForm, setIsSubmitting, goToBlock, setSubmissionErr }) => {
+                                onSubmit={(data, { completeForm, setIsSubmitting }) => {
                                     setTimeout(() => {
                                         setIsSubmitting(false);
                                         completeForm();
