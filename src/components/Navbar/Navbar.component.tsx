39c796478b0b86337d7fadfa530a18f78bab5516
HARSHAVARDHAN2K <vardhanvardhanthi@gmail.com>
2023-05-19 21:14:31 +0530

    navbar stylling going on

--- src/components/Navbar/Navbar.component.tsx
+++ src/components/Navbar/Navbar.component.tsx
@@ -47,7 +47,7 @@ export const Navibar = () => {
               fontWeight: "bold",
               fontSize: "18px",
               backgroundColor: "white",
-              
+              width: "15vh",
               color: "lightskyblue",
             }}
             />
@@ -57,42 +57,5 @@ export const Navibar = () => {
         {/* </div> */}
       </Container>
     </Navbar>
-    // <Box className="w3-animate-opacity App">
-    //   <nav className="navbar">
-    //     <Box>
-    //       <Box className="w3-animate-opacity">
-    //         <p className="logo">
-    //           <i
-    //             className="fa-solid fa-fingerprint"
-    //             style={{ color: "white", paddingRight: "15px" }}
-    //           ></i>
-    //           Proof-Of-Identity
-    //         </p>
-    //       </Box>
-    //     </Box>
-    //     <ul className="nav-menu">
-    //       {MenuData.map((menu, index) => {
-    //         return (
-    //           <li key={index}>
-    //             <Link to={menu.url} className={menu.cName}>
-    //               <i className={menu.icon} style={{ paddingRight: "10px" }}></i>
-    //               {menu.title}
-    //             </Link>
-    //           </li>
-    //         );
-    //       })}
-    //       <WalletMultiButton
-    //         style={{
-    //           fontFamily: "Roboto Mono,monospace",
-    //           fontWeight: "bolder",
-    //           fontSize: "18px",
-    //           backgroundColor: "white",
-    //           color: "lightskyblue",
-    //           position: "relative",
-    //         }}
-    //       />
-    //     </ul>
-    //   </nav>
-    // </Box>
   );
 };
